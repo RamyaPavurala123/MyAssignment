8c4aa4b2149d62fce5962afd5d31fc52027d8438
Ramya-Pavurala <160400685+Ramya-Pavurala@users.noreply.github.com>
2025-03-15 14:05:26 +0530

    task

--- src/MainContent.js
+++ src/MainContent.js
@@ -5,7 +5,7 @@ function MainContent() {
   return (
     <div className="main-content">
       <div className="image-container">
-        <img src={`${process.env.PUBLIC_URL}/images/statue.png`} alt="Statue" />
+      <img src="/images/statue.png" alt="Statue" />
       </div>
     </div>
   );
